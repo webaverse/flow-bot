245a16c68472bfd3e77e300569f288fd69849f8f
Avaer Kazmer <a@modules.io>
2020-10-07 03:28:10 -0400

    Support monomint in FT mint

--- index.js
+++ index.js
@@ -428,8 +428,11 @@ Help
             } else {
               message.channel.send('no such user');
             }
-          } else if (split[0] === prefix + 'mint' && split.length >= 2 && !isNaN(parseFloat(split[1])) && message.author.id === adminUserId && message.attachments.size === 0) {
-            const amount = parseFloat(split[1]);
+          } else if (split[0] === prefix + 'mint' && message.author.id === adminUserId && message.attachments.size === 0) {
+            let amount = parseFloat(split[1]);
+            if (isNaN(amount)) {
+              amount = 1;
+            }
 
             let {mnemonic, addr} = await _getUser();
             if (!mnemonic) {
